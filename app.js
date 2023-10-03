abb192880c54589b2fd73617a6f90547c4dfc932
spykelionel <ndilionel6@gmail.com>
2023-10-03 14:17:58 +0100

    simple node-application example

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
-// console.log("Hello World!")
-console.log(process.env.Password)
+const http = require('http')
 
-// dotenv
+const port = 5000
+const hostname = '127.0.0.1'
+
+const server = http.createServer((request, respond)=>{
+    respond.statusCode = 200
+    respond.setHeader("Content-Type", 'text/plain')
+    respond.end("Hello world!")
+})
+
+server.listen(port, hostname, ()=>{
+    console.log(`Server is runnit at http://${hostname}:${port}`)
+})
