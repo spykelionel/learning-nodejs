c6b65f76289795034b72ef363c8ee709f3a247bc
spykelionel <ndilionel6@gmail.com>
2023-10-04 16:54:39 +0100

    intro to express

--- app.js
+++ app.js
@@ -1,38 +1,24 @@
-const { json } = require('body-parser')
-const http = require('http')
-const { hostname } = require('os')
+const express = require("express");
+const { createServer, request } = require("http");
 
-const server = http.createServer((request, response)=>{
-   console.log(request.url)
-   console.log(request.method)
-   const html = `
-   <!DOCTYPE html>
-   <html lang="en">
-   <head>
-       <meta charset="UTF-8">
-       <meta name="viewport" content="width=device-width, initial-scale=1.0">
-       <title>Document</title>
-   </head>
-   <body>
-       <h1>This is the response</h1>
-       
-   </body>
-   </html>
-   `
+const app = express();
 
-   const person = {
-    name: "Chun",
-    age: "23",
-    gender: "F"
-   }
-   const _response = {
-    method: request.method,
-    url: request.url
-   }
-
-   response.setHeader("Content-Type", "application/json")
-   response.end(JSON.stringify(person))
-   
+// http://localhost:5000/
+app.get("/", (request, response)=>{
+// handle every request at /
+    response.status(201).send({
+        name: "Lin",
+        age: 34
+    })
 })
 
-server.listen(5000, "127.0.0.1", ()=>console.log("server is running at http://localhost:5000"))
+// http://localhost:5000/users
+app.get("/users", (request, response)=>{
+    // handle every request at /users
+    response.send("This is the users endpoint")
+})
+
+app.listen(5000, () => {
+  console.log("App is running on port 5000");
+});
+
