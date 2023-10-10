014a0a69b3a58aa3e9d2dce9a44f54f80a52fad0
spykelionel <ndilionel6@gmail.com>
2023-10-10 15:51:13 +0100

    mongodb

--- app.js
+++ app.js
@@ -6,8 +6,49 @@ dotenv.config();
 
 const app = express();
 
+const todoRouter = express.Router()
+
+const allTodos = async (req, res) => {
+    await Todo.find({})
+    .then(todos => {
+        return res.status(200).json({
+            todos
+        })
+    })
+    .catch(error=>console.log(error))
+}
+
+const createTodo = async (req, res) => {
+    // Check if todotext exist, then send a 409 - for a dublicate,
+    // Todo.findOne({text: req.body.text}) || If todo, return 409, else 
+    // return 409.
+    const newTodo = new Todo({text: req.body.text})
+    await newTodo.save()
+        .then(todo => {
+            // if(todo) {
+            //     /// return 409
+            // } else {
+            //     // create a todo
+            // }
+            return res.status(201).json({
+                todo
+            })
+        })
+        .catch(error=>console.log(error))
+}
+
+todoRouter.get('/', allTodos)
+.post("/create", createTodo)
+// .delete("/delete/:todoId", deleteTodo)
+
+// Add delete todo Todo to the router chain above.
+
+// http://localhost:8000/todos/create
+
+
 app.use(express.json())
 app.use(express.urlencoded({ extended: true, }))
+app.use("/todos", todoRouter)
 
 
 let users = [
@@ -28,13 +69,52 @@ let users = [
     }
 ]
 
+
+const todoSchema = new mongoose.Schema({
+    text: {
+        type: String,
+        required: true
+    },
+    isComplete: {
+        type: String,
+        required: false,
+        default: false
+    },
+    description: {
+        type: String,
+        required: true,
+        default: "A todo"
+    }
+}, { timestamps: true })
+
+const Todo = mongoose.model("Todo", todoSchema)
+
+
 mongoose.connect(process.env.MONGODBURL, { dbName: "first-app" })
-    .then(success => {
-        console.log("Mongodb is now running")
-    })
-    .catch(error => {
-        console.log("Error connecting to DB: ", error)
-    })
+.then(success => {
+    console.log("Mongodb is now running")
+})
+.catch(error => {
+    console.log("Error connecting to DB: ", error)
+})
+
+
+// app.get("/todos", allTodos)
+
+// app.post("/todos/create", createTodo)
+
+app.delete("/todos/delete/:todoId", async (req, res) => {
+    const {todoId} = req.params
+    await Todo.findOneAndDelete({_id: todoId})
+        .then(todo => {
+            return res.status(201).json({
+                todo
+            })
+        })
+        .catch(error=>console.log(error))
+})
+
+// -> Ge a single todo....
 
 app.get("/", (req, res) => {
     res.json({
@@ -90,7 +170,7 @@ app.put("/users/:username", (req, res) => {
 
         // modify
         let currentUser = user
-        users = users.filter(u=>u.name!=username)
+        users = users.filter(u => u.name != username)
 
         currentUser.name = name
         users = [...users, currentUser]
@@ -143,6 +223,16 @@ app.listen(8000, '127.0.0.1', (err) => {
 
 
 /*
+
+
+todo = {
+    id: ...,
+    text: ....,
+    isComplete: ...,
+    description: ....,
+    date_created: ...,
+    date_updated: ...,
+}
 
 
 */
